e7a2d3820f116fc0af4cb77466d2b47b403f37d4
idevops-academy <71696537+idevops-academy@users.noreply.github.com>
2021-10-27 09:33:10 +0300

    Create deploy.sh

---
+++ nginx/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sudo yum update -y
+
+#Install nginx on Amazon Linux
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+#Install git
+sudo yum install git -y
+
+#Install nodejs
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install nodejs -y
+
+#Install process manager to run the app
+sudo npm install pm2 -g
+
+#create directory for cloning the app
+sudo mkdir -p /var/myapp
+cd /var/myapp
+
+#clone the application from github
+sudo git clone https://github.com/idevops-academy/iConnect-Demo.git (authentication needed)
+cd iConnect-Demo/
+
+#install application dependencies and start the app
+sudo npm install
+pm2 start --name iconnect npm -- start
+
+#copy the nginx configuration to forward the requests to the app
+sudo cp nginx/sysmon.conf /etc/nginx/conf.d/
+
+#restart nginx
+sudo systemctl restart nginx
+
